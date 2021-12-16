22c1f816b7d6a4d04f3b05c081f0b49fb062220b
anssiketomaki <anssi.ketomaki@gmail.com>
2021-12-16 23:52:59 +0200

    osa2 haluttu ratkaisu

--- osa1/uusisovellus/src/App.js
+++ osa1/uusisovellus/src/App.js
@@ -12,9 +12,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.content}
-      </p>
+      {<Part part = {props.part1} exercises = {props.exercises1}/>}
+      {<Part part = {props.part2} exercises = {props.exercises2}/>}
+      {<Part part = {props.part3} exercises = {props.exercises3}/>}
     </div>
   )
 }
@@ -47,9 +47,9 @@ const App = () => {
     return (
       <div> 
         <Header course = {course} />
-        <Content content= {<Part part = {part1} exercises = {exercises1}/>}/>
-        <Content content= {<Part part = {part2} exercises = {exercises2}/>}/>
-        <Content content= {<Part part = {part3} exercises = {exercises3}/>}/>
+        <Content part1 = {part1} exercises1 = {exercises1}/>
+        <Content part2 = {part2} exercises2 = {exercises2}/>
+        <Content part3 = {part3} exercises3 = {exercises3}/>
         <Total total={exercises1 + exercises2 + exercises3}/>
       </div>
     )
