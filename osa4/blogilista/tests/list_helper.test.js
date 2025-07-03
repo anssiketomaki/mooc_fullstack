f2508a9b9305c320306da81b645aafc300b57d9f
Anssi Ketomäki <anssi.ketomaki@tuni.fi>
2025-07-03 03:06:43 +0300

    done 4.5

--- osa4/blogilista/tests/list_helper.test.js
+++ osa4/blogilista/tests/list_helper.test.js
@@ -2,15 +2,7 @@ const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
-test('dummy returns one', () => {
-  const blogs = []
-
-  const result = listHelper.dummy(blogs)
-  assert.strictEqual(result, 1)
-})
-
-describe('total likes', () => {
-    const listWithOneBlog = [
+const listWithOneBlog = [
         {
         _id: '5a422aa71b54a676234d17f8',
         title: 'Go To Statement Considered Harmful',
@@ -71,6 +63,15 @@ describe('total likes', () => {
         }  
     ]
 
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+    
     test('when list has only one blog equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         assert.strictEqual(result, 5)
@@ -79,5 +80,17 @@ describe('total likes', () => {
     test('when list has multiple blogs the sum of likes is correct', () => {
         const result = listHelper.totalLikes(listWithManyBlogs)
         assert.strictEqual(result, 36)
+    })
+})
+
+describe('most liked', () => {
+    test('most liked is found from list of one', () => {
+        const result = listHelper.favoriteBlog(listWithOneBlog)
+        assert.strictEqual(result.likes, 5)
+    })
+    
+    test('most liked blog is found out of many blogs', () => {
+        const result = listHelper.favoriteBlog(listWithManyBlogs)
+        assert.strictEqual(result.likes, 12)
     })
 })
--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -6,8 +6,16 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    return blogs.reduce((maxLikesBlog, currentBlog) =>{
+        return currentBlog.likes > maxLikesBlog.likes
+            ? currentBlog
+            : maxLikesBlog 
+    })
+}
 
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
